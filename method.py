e06803ef37fb1d32c7343f0aeb0056b90e9d669a
Krithika Balu <krithikb@thoughtworks.com>
2019-03-03 13:43:11 +0530

    [Krithika] Update example for methods in python

--- method.py
+++ method.py
@@ -3,33 +3,33 @@ def multiplication_tables(number, times):
 		print("{} * {} = {}".format(number, time, number*time))
 
 print("\nUsing positional arguments")
-multiplication_tables(7,10)
+multiplication_tables(7,5)
 print("-"*90)
 
 print("\nUsing named parameters")
-multiplication_tables(times=10, number=7)
+multiplication_tables(times=5, number=7)
 print("-"*90)
 
 #using args
 def multiplication_tables(*args):
-	print(args)
+	print("args: {}".format(args))
 	number = args[0]
 	times = args[1]
 	for time in range(1, times+1):
 		print("{} * {} = {}".format(number, time, number*time))
 print("\nUsing *args")
-multiplication_tables(7,10)
+multiplication_tables(7,5)
 print("-"*90)
 
 
 #using kwargs
 def multiplication_tables(**kwargs):
-	print(kwargs)
+	print("kwargs: {}".format(kwargs))
 	number = kwargs["number"]
 	times = kwargs["times"]
 	for time in range(1, times+1):
 		print("{} * {} = {}".format(number, time, number*time))
 
 print("\nUsing **kwargs")
-multiplication_tables(number=7, times=10)
+multiplication_tables(number=7, times=5)
 print("-"*90)
