de4ea09e6e76496b2be301f01ca039f23e4c994a
Krithika Balu <krithikb@thoughtworks.com>
2019-03-03 13:17:59 +0530

    [Krithika] Update lambda.py example

--- lambda.py
+++ lambda.py
@@ -1,17 +1,32 @@
-import functools;
+numbers = [100, 957, 423, 65, 350]
 
-numbers = [1948, 957, 423, 65, 350]
-print_all_numbers = lambda x: print("All numbers: %s" %(numbers))
-print_all_numbers(numbers)
+# traditional way
+def is_even(number):
+	return number%2 == 0
 
-even_numbers = list(filter(lambda x: x%2==0, numbers))
-print("Even numbers {}".format(even_numbers))
+def even_numbers(numbers):
+	even_numbers = []
+	for number in numbers:
+		if is_even(number):
+			even_numbers.append(number)
+	return even_numbers
+
+print("Even numbers using traditional method {}".format(even_numbers(numbers)))
+
+#lambda way
+even_numbers = list(filter(lambda x: x%2 == 0, numbers))
+print("Even numbers using lambda filter {}".format(even_numbers))
 
 multiplied_by_10 = list(map(lambda x: x*10, numbers))
 print("Multiplied by 10 {}".format(multiplied_by_10))
 
+import functools;
 sum_of_all = functools.reduce(lambda x,y : x+y, numbers)
 print("Sum of all numbers {}".format(sum_of_all))
 
 least_of_all = functools.reduce(lambda x,y : x if x<y else y, numbers)
 print("Least number {}".format(least_of_all))
+
+numbers = [1948, 957, 423, 65, 350]
+print_all_numbers = lambda x: print("All numbers: %s" %(numbers))
+print_all_numbers(numbers)
